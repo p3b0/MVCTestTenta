5d8b1aebdc81eef4d4473b276cae29713b5782e3
Peter Borg <Peter.Borg.spo16@ecusolna.onmicrosoft.com>
2018-01-24 09:59:04 +0100

    UnitPrice should be less than or equal to 1000000

--- MVCTestTenta/Models/Product.cs
+++ MVCTestTenta/Models/Product.cs
@@ -9,8 +9,9 @@ namespace MVCTestTenta.Models
     public class Product
     {
         public int ProductId { get; set; }
-        [StringLength(60,MinimumLength =3)]
+        [StringLength(60,MinimumLength = 3)]
         public string Name { get; set; }
+        [Range(0, 1000000)]
         public int UnitPrice { get; set; }
         public DateTime ReleaseDate { get; set; }
         public bool IsDeleted { get; set; }
