2dfe5590e1a2beee7335db7b98c4813d3ce43e3f
Peter Borg <Peter.Borg.spo16@ecusolna.onmicrosoft.com>
2018-01-24 09:41:20 +0100

    Add project files.

---
+++ MVCTestTenta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCTestTenta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
