7ade3a8e544290416bbeb8237f70d7ac347a9ee5
Peter Borg <Peter.Borg.spo16@ecusolna.onmicrosoft.com>
2018-01-24 09:53:31 +0100

    Name is at least 3 characters long

--- MVCTestTenta/Models/Product.cs
+++ MVCTestTenta/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace MVCTestTenta.Models
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        [StringLength(60,MinimumLength =3)]
         public string Name { get; set; }
-        public int unitPrice { get; set; }
+        public int UnitPrice { get; set; }
         public DateTime ReleaseDate { get; set; }
         public bool IsDeleted { get; set; }
     }
