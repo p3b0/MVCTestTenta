0e0d9695cf5aba5943437371c5bccfdc06fed034
Peter Borg <Peter.Borg.spo16@ecusolna.onmicrosoft.com>
2018-01-25 20:21:37 +0100

    Added CultureCode to ApplicationUser

--- MVCTestTenta/Models/ApplicationUser.cs
+++ MVCTestTenta/Models/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace MVCTestTenta.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string CultureCode { get; set; }
     }
 }
